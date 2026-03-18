7bd377fcc281e776c6e6bf68f67b17a3a5e16e65
SergioGTS <sergunya87@ya.ru>
2026-03-18 23:02:52 +0300

    the working code has been written, the app is working

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int initialBalance = 100;
+        int addend = 1200;
+
+        int bonus = 0;
+        if (addend > 1000) {
+            bonus = addend / 100;
+        }
+            int finalBalance = initialBalance + addend + bonus;
+            System.out.println("итог: " + finalBalance);
+            System.out.println("Бонусы " + bonus);
+        }
+}
